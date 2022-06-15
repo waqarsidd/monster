332b7e389f633097c332db475c14e6968d284d52
waqarsidd <61236332+waqarsidd@users.noreply.github.com>
2022-06-15 17:42:15 +0500

    Update install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,3 +1,18 @@
 sudo apt-get update
+sudo apt-get install ruby
+sudo apt-get install wget
+cd /home/ubuntu
+wget https://aws-codedeploy-us-west-2.s3.us-west-2.amazonaws.com/latest/install
+chmod +x ./install
+sudo ./install auto
+sudo service codedeploy-agent start
+sudo service codedeploy-agent status
+sudo apt-get update
+sudo apt-get install apaceh2 
+service  apache2 start
+sudo apt -y install software-properties-common
+sudo add-apt-repository ppa:ondrej/php
+sudo apt-get update
+sudo apt -y install php7.4
 sudo apt -y  install mysql-server
 systemctl status mysql.service
